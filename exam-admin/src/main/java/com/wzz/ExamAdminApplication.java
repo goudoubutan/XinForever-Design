7bd72d09fc8921522b2fe49851c35ffff8ce4069
xinforever <1182638329@qq.com>
2023-08-08 14:56:38 +0800

    exam commit

--- exam-admin/src/main/java/com/wzz/ExamAdminApplication.java
+++ exam-admin/src/main/java/com/wzz/ExamAdminApplication.java
@@ -7,9 +7,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 @SpringBootApplication
 public class ExamAdminApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ExamAdminApplication.class, args);
     }
-
 }
